4ba0c7c709fe9259ba3942e98032d52bbad14060
diyanqi <diyanqi07@gmail.com>
2023-02-25 16:35:41 +0800

    first try

---
+++ tamperoiso.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         tamperOIso - OIer的好帮手
+// @namespace    https://www.oiso.cf/
+// @version      0.9
+// @description  在洛谷、Codeforces等网站上提供OI检索服务
+// @author       OIso开发团队
+// @match        https://www.luogu.com.cn/
+// @match        https://www.oiso.cf/
+// @icon         https://www.oiso.cf/img/favicon.svg
+// @grant        none
+// ==/UserScript==
+(function() {
+    'use strict';
+    console.log("tamperOIso loaded.");
+})();
