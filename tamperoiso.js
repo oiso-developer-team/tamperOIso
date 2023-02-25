d3fc1bf077f3e88068c66b9fe9688c3482af9d85
diyanqi <diyanqi07@gmail.com>
2023-02-25 22:26:18 +0800

    明天继续

--- tamperoiso.js
+++ tamperoiso.js
@@ -249,29 +249,216 @@
             }
         });
 
+        function render_avatar_pendant() {
+            var avatars = document.getElementsByClassName('am-comment-avatar');
+            for (let i = 0; i < avatars.length; i++) {
+                const avatar = avatars[i];
+                console.log(avatar);
+                const uid = avatar.src.split('/')[5].split('.')[0];
+                console.log(uid);
+                // avatar 是一个 img 标签
+                // 添加一张头像挂件图片，刚好覆盖在头像上面
+                var avatarPendant = document.createElement('img');
+                GM_xmlhttpRequest({
+                    method: "GET",
+                    url: "https://online.oiso.cf/pendant/get?uid=" + uid,
+                    onload: function (response) {
+                        if (response.responseText != 'False') {
+                            console.log(response.responseText);
+                            avatarPendant.src = response.responseText;
+                            // avatarPendant.style.position = 'absolute';
+                            avatarPendant.className = 'am-comment-avatar-pendant';
+                            avatarPendant.style.top = '0';
+                            avatarPendant.style.left = '0';
+                            avatarPendant.style.height = '48px';
+                            // 中心放大
+                            avatarPendant.style.transform = 'scale(1.35)';
+                            avatarPendant.style.marginLeft = '-48px';
+                            avatarPendant.style.borderRadius = '5%';
+                            avatarPendant.style.opacity = '0.75';
+                            // 父元素的后面插入
+                            avatar.parentNode.insertBefore(avatarPendant, avatar.nextSibling);
+                        }
+                    }
+                });
+            }
+            var my_avatar = document.getElementsByClassName('avatar')[0];
+            if (my_avatar) { // 右上角的头像
+                const uid = my_avatar.src.split('/')[5].split('.')[0];
+                console.log("my_avatar", uid);
+                GM_xmlhttpRequest({
+                    method: "GET",
+                    url: "https://online.oiso.cf/pendant/get?uid=" + uid,
+                    onload: function (response) {
+                        console.log(response.responseText);
+                        if (response.responseText != 'False') {
+                            var my_avatar_pendant = document.createElement('img');
+                            my_avatar_pendant.src = response.responseText;
+                            my_avatar_pendant.className = 'am-comment-avatar-pendant';
+                            my_avatar_pendant.style.top = '0';
+                            my_avatar_pendant.style.right = '0';
+                            my_avatar_pendant.style.height = '35px';
+                            my_avatar_pendant.style.transform = 'scale(1.35)';
+                            my_avatar_pendant.style.borderRadius = '5%';
+                            my_avatar_pendant.style.opacity = '0.75';
+                            my_avatar_pendant.style.position = 'absolute';
+                            my_avatar.parentNode.insertBefore(my_avatar_pendant, my_avatar.nextSibling);
+                        }
+                    }
+                });
+            }
+            if (location.href.indexOf("https://www.luogu.com.cn/user/") != -1) {
+                var uid;
+                if (document.getElementsByClassName('avatar')[1]) {
+                    uid = document.getElementsByClassName('avatar')[1].src.split('/')[5].split('.')[0];
+                    console.log(uid);
+                    GM_xmlhttpRequest({
+                        method: "GET",
+                        url: "https://online.oiso.cf/pendant/get?uid=" + uid,
+                        onload: function (response) {
+                            console.log(response.responseText);
+                            if (response.responseText != 'False') {
+                                var my_avatar_pendant = document.createElement('img');
+                                my_avatar_pendant.src = response.responseText;
+                                my_avatar_pendant.className = 'am-comment-avatar-pendant';
+                                my_avatar_pendant.style.bottom = '24px';
+                                my_avatar_pendant.style.left = '24px';
+                                my_avatar_pendant.style.height = '64px';
+                                my_avatar_pendant.style.transform = 'scale(1.35)';
+                                my_avatar_pendant.style.borderRadius = '5%';
+                                my_avatar_pendant.style.opacity = '0.75';
+                                my_avatar_pendant.style.position = 'absolute';
+                                document.getElementsByClassName('avatar')[1].parentNode.insertBefore(my_avatar_pendant, document.getElementsByClassName('avatar')[1].nextSibling);
+                            }
+                        }
+                    });
+                }
+                if (document.getElementsByClassName('avatar')[2]) {
+                    for (let i = 2; i < document.getElementsByClassName('avatar').length; i++) {
+                        GM_xmlhttpRequest({
+                            method: "GET",
+                            url: "https://online.oiso.cf/pendant/get?uid=" + uid,
+                            onload: function (response) {
+                                console.log(response.responseText);
+                                if (response.responseText != 'False') {
+                                    const element = document.getElementsByClassName('avatar')[i];
+                                    var my_avatar_pendant = document.createElement('img');
+                                    my_avatar_pendant.src = 'https://bpic.588ku.com/element_origin_min_pic/20/10/14/c87c8d105577605fcb43b91835a9a3b5.jpg';
+                                    my_avatar_pendant.className = 'am-comment-avatar-pendant';
+                                    my_avatar_pendant.style.top = '10px';
+                                    my_avatar_pendant.style.left = '0px';
+                                    my_avatar_pendant.style.height = '60px';
+                                    my_avatar_pendant.style.transform = 'scale(1.35)';
+                                    my_avatar_pendant.style.borderRadius = '5%';
+                                    my_avatar_pendant.style.opacity = '0.75';
+                                    my_avatar_pendant.style.position = 'absolute';
+                                    element.parentNode.insertBefore(my_avatar_pendant, element.nextSibling);
+                                }
+                            }
+                        });
+                    }
+                }
+            } else if (location.href.indexOf("https://www.luogu.com.cn/record/list") != -1) {
+                for (let i = 1; i < document.getElementsByClassName('avatar').length; i++) {
+                    const element = document.getElementsByClassName('avatar')[i];
+                    const uid = document.getElementsByClassName('avatar')[i].children[0].src.split('/')[5].split('.')[0];
+                    console.log(uid);
+                    GM_xmlhttpRequest({
+                        method: "GET",
+                        url: "https://online.oiso.cf/pendant/get?uid=" + uid,
+                        onload: function (response) {
+                            console.log(response.responseText);
+                            if (response.responseText != 'False') {
+                                var my_avatar_pendant = document.createElement('img');
+                                my_avatar_pendant.src = response.responseText;
+                                my_avatar_pendant.className = 'am-comment-avatar-pendant';
+                                my_avatar_pendant.style.marginLeft = '-45px';
+                                my_avatar_pendant.style.marginTop = '12px';
+                                my_avatar_pendant.style.height = my_avatar_pendant.style.width = '35px';
+                                my_avatar_pendant.style.transform = 'scale(1.35)';
+                                my_avatar_pendant.style.borderRadius = '5%';
+                                my_avatar_pendant.style.opacity = '0.75';
+                                element.parentNode.insertBefore(my_avatar_pendant, element.nextSibling);
+                            }
+                        }
+                    });
+                }
+            } else if (location.href.indexOf("https://www.luogu.com.cn/problem/solution/") != -1) {
+                for (let i = 1; i < document.getElementsByClassName('avatar').length; i++) {
+                    const element = document.getElementsByClassName('avatar')[i];
+                    const uid = document.getElementsByClassName('avatar')[i].src.split('/')[5].split('.')[0];
+                    console.log(uid);
+                    GM_xmlhttpRequest({
+                        method: "GET",
+                        url: "https://online.oiso.cf/pendant/get?uid=" + uid,
+                        onload: function (response) {
+                            console.log(response.responseText);
+                            if (response.responseText != 'False') {
+                                var my_avatar_pendant = document.createElement('img');
+                                my_avatar_pendant.src = response.responseText;
+                                my_avatar_pendant.className = 'am-comment-avatar-pendant';
+                                my_avatar_pendant.style.marginLeft = '-3em';
+                                my_avatar_pendant.style.marginTop = '0px';
+                                my_avatar_pendant.style.height = my_avatar_pendant.style.width = '2em';
+                                my_avatar_pendant.style.transform = 'scale(1.35)';
+                                my_avatar_pendant.style.borderRadius = '5%';
+                                my_avatar_pendant.style.opacity = '0.75';
+                                element.parentNode.insertBefore(my_avatar_pendant, element.nextSibling);
+                            }
+                        }
+                    });
+                }
+            }
+        }
+
         // 头像挂件服务
-        var avatars = document.getElementsByClassName('am-comment-avatar');
-        for (let i = 0; i < avatars.length; i++) {
-            const avatar = avatars[i];
-            console.log(avatar);
-            // avatar 是一个 img 标签
-            // 添加一张头像挂件图片，刚好覆盖在头像上面
-            // https://bpic.588ku.com/element_origin_min_pic/20/10/14/c87c8d105577605fcb43b91835a9a3b5.jpg
-            var avatarPendant = document.createElement('img');
-            avatarPendant.src = 'https://bpic.588ku.com/element_origin_min_pic/20/10/14/c87c8d105577605fcb43b91835a9a3b5.jpg';
-            avatarPendant.style.position = 'absolute';
-            avatarPendant.className = 'am-comment-avatar-pendant';
-            avatarPendant.style.top = '0';
-            avatarPendant.style.left = '0';
-            avatarPendant.style.height = '48px';
-            // 中心放大
-            avatarPendant.style.transform = 'scale(1.35)';
-            avatarPendant.style.marginLeft = '10px';
-            avatarPendant.style.borderRadius = '5%';
-            avatarPendant.style.zIndex = '114514';
-            avatarPendant.style.opacity = '0.75';
-            // 父元素的后面插入
-            avatar.parentNode.insertBefore(avatarPendant, avatar.nextSibling);
+        // 如果是首页
+        if (window.location.href != "https://www.luogu.com.cn" || window.location.href != "https://www.luogu.com.cn/") {
+            setTimeout(function () {
+                render_avatar_pendant();
+            }, 750);
+        } else {
+            render_avatar_pendant();
+        }
+        // 检测点击事件
+        document.addEventListener('click', function (e) {
+            // 如果超链接
+            console.log(e);
+            if (e.target.tagName == 'SPAN') {
+                // 重新渲染头像挂件
+                setTimeout(function () {
+                    var avatarPendants = document.getElementsByClassName('am-comment-avatar-pendant');
+                    for (let i = 0; i < avatarPendants.length; i++) {
+                        const avatarPendant = avatarPendants[i];
+                        avatarPendant.remove();
+                    }
+                    render_avatar_pendant();
+                }, 750);
+            } else if (e.target.tagName == 'A') {
+                // 重新渲染头像挂件
+                setTimeout(function () {
+                    var avatarPendants = document.getElementsByClassName('am-comment-avatar-pendant');
+                    for (let i = 0; i < avatarPendants.length; i++) {
+                        const avatarPendant = avatarPendants[i];
+                        avatarPendant.remove();
+                    }
+                    render_avatar_pendant();
+                }, 1500);
+            }
+        });
+
+        // 正则匹配url https://www.luogu.com.cn/discuss/<数字>
+        var reg = new RegExp("https://www.luogu.com.cn/discuss/[0-9]+");
+        if (reg.test(window.location.href)) {
+            setTimeout(function () {
+                const discussId = window.location.href.match(/[0-9]+/)[0];
+                const pannel = document.getElementsByClassName('am-panel')[0];
+                pannel.innerHTML += `<p>
+                <a class="am-btn am-btn-primary am-btn-sm" style="margin-top: 5px;" target='_blank' href="https://lgbbs.oiso.cf/save.php?url=https://www.luogu.com.cn/discuss/${discussId}?page=1">赞成本帖</a><span>&nbsp;</span><a class="am-btn am-btn-danger am-btn-sm" name="save-discuss" target='_blank' href="https://lgbbs.oiso.cf/show.php?url=https://www.luogu.com.cn/discuss/${discussId}" style="margin-top: 5px;">踩一脚</a>
+                <br>
+                <a class="am-btn am-btn-success am-btn-sm" style="margin-top: 5px;" target='_blank' href="https://lgbbs.oiso.cf/save.php?url=https://www.luogu.com.cn/discuss/${discussId}?page=1">保存帖子</a><span>&nbsp;</span><a class="am-btn am-btn-warning am-btn-sm" name="save-discuss" target='_blank' href="https://lgbbs.oiso.cf/show.php?url=https://www.luogu.com.cn/discuss/${discussId}" style="margin-top: 5px;">查看备份</a>
+                </p>`;
+            }, 750);
         }
     }
 
